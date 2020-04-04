984ba074417eea7e6fbe7e6e821f410a1a43e796
krimzixythe <krimzixythe@gmail.com>
2020-04-04 13:08:01 -0400

    Added comments.

--- program.h
+++ program.h
@@ -3,9 +3,7 @@
 
 #include <QDialog>
 
-namespace Ui {
-class Program;
-}
+namespace Ui { class Program; }
 
 class Program : public QDialog
 {
@@ -14,11 +12,14 @@ class Program : public QDialog
 public:
     explicit Program(QWidget *parent = nullptr);
     ~Program();
-    int getRand(int min, int max);
+    
+    // For simulating power consumption.
+    int getRand(int min, int max); 
     int batteryStatus();
-    void setBattery(int b, int cntr);
-    void setPwr(int p);
+    void setBattery(int b, int cntr); // Modifies battery charge.
+    void setPwr(int p); // For simulating frequency.
     void batteryHandler();
+    
     void handler(int s);
     void program_test();
     void disable_prog();
@@ -30,37 +31,30 @@ public slots:
     void cnt();
 
 private slots:
-    void on_battery_lvl_valueChanged(int value);
+    void on_battery_lvl_valueChanged(int value); // For simulating power consumption.
+    // UI
     void on_off_button_clicked();
-
     void on_return_2_clicked();
-
     void on_b1_clicked();
-
     void on_b2_clicked();
-
     void on_b3_clicked();
-
     void on_b4_clicked();
-
     void on_b5_clicked();
-
     void on_b6_clicked();
-
     void on_b7_clicked();
-
     void on_b8_clicked();
 
 private:
     Ui::Program *ui;
     QTimer *timer;
     QTimer *counter;
+    // For simulating power consumption.
     int battery;
     int battery_cntr = 0;
-    int pwr_usage = 0;
+    int pwr_usage = 0; // For determining frequency.
     int answer = 0;
     int length = 0;
 };
 
-#endif // Program_H
+#endif // PROGRAM_H
 
