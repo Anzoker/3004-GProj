804c3147eae841659de884eeba13db38fb0b67b6
krimzixythe <krimzixythe@gmail.com>
2020-04-04 18:33:01 -0400

    Update screening_confirm.cpp

--- screening_confirm.cpp
+++ screening_confirm.cpp
@@ -4,23 +4,18 @@
 screening_confirm::screening_confirm(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::screening_confirm)
-{
-    ui->setupUi(this);
-}
 
-screening_confirm::~screening_confirm()
-{
-    delete ui;
-}
+{ ui->setupUi(this); }
 
-void screening_confirm::on_pushButton_2_clicked()
-{
+screening_confirm::~screening_confirm() { delete ui; }
+
+// Button for "no".
+void screening_confirm::on_pushButton_2_clicked() {
     emit valueChanged(0);
     close();
 }
 
-void screening_confirm::on_yes_clicked()
-{
+void screening_confirm::on_yes_clicked() {
     emit valueChanged(1);
     close();
 }
