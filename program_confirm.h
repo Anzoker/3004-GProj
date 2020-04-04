7d65df631f2c37c1c439475b99df795ac806acac
krimzixythe <krimzixythe@gmail.com>
2020-04-04 11:25:58 -0400

    Added comments.

--- program_confirm.h
+++ program_confirm.h
@@ -4,27 +4,27 @@
 #include <QDialog>
 
 namespace Ui {
-class program_confirm;
+class Program_Confirm;
 }
 
-class program_confirm : public QDialog
+class Program_Confirm : public QDialog
 {
     Q_OBJECT
 
 public:
-    explicit program_confirm(QWidget *parent = nullptr);
-    void setText(int s);
-    ~program_confirm();
+    explicit Program_Confirm(QWidget *parent = nullptr);
+    ~Program_Confirm();
+    void setText(int s); // Displays text to user after a program is selected.
 
 private slots:
-    void on_pushButton_2_clicked();
+    void on_pushButton_2_clicked(); // Button for "no".
     void on_yes_clicked();
 
 private:
-    Ui::program_confirm *ui;
+    Ui::Program_Confirm *ui;
 
 signals:
-    void valueChanged(int newValue);
+    void valueChanged(int newValue); // "Program" checks for the value emitted by this signal.
 };
 
 #endif // PROGRAM_CONFIRM_H
