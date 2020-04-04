cb9cfe71d8bf8f38a1a508f2f4eb25ac5de7e5b8
krimzixythe <krimzixythe@gmail.com>
2020-04-04 19:17:43 -0400

    Added comments to settings.h

--- settings.h
+++ settings.h
@@ -3,9 +3,7 @@
 
 #include <QDialog>
 
-namespace Ui {
-class Settings;
-}
+namespace Ui { class Settings; }
 
 class Settings : public QDialog
 {
@@ -14,6 +12,7 @@ class Settings : public QDialog
 public:
     explicit Settings(QWidget *parent = nullptr);
     ~Settings();
+    // For simulating power consumption.
     int getRand(int min, int max);
     int batteryStatus();
     void setBattery(int b, int cntr);
@@ -24,16 +23,17 @@ public slots:
     void time();
 
 private slots:
-    void on_battery_lvl_valueChanged(int value);
-    void on_off_button_clicked();
+    void on_battery_lvl_valueChanged(int value); // For simulating power consumption.
+    void on_off_button_clicked(); // UI.
 
 private:
     Ui::Settings *ui;
     QTimer *timer;
+    // For simulating power consumption.
     int battery;
     int battery_cntr = 0;
     int pwr_usage = 0;
 };
 
-#endif // Settings_H
+#endif // SETTINGS_H
 
